685b417f1a8cce67c8a6985c2c119e24648a7ba9
Sumit <itzchinuji7@gmail.com>
2026-02-22 21:17:49 +0530

    Added Farmer Dashboard UI with crop cards

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,5 @@
-import Navbar from "./components/Navbar"
-
 function App() {
-  return (
-    <div>
-      <Navbar />
-
-      <div className="hero">
-        <h1>Fair & Direct Crop Marketplace</h1>
-        <p>Connecting Farmers with NGOs & Bulk Buyers</p>
-
-        <div className="buttons">
-          <button>I am a Farmer</button>
-          <button>I am a Buyer</button>
-        </div>
-      </div>
-    </div>
-  )
+  return <h1>App Running 😏</h1>
 }
 
 export default App
